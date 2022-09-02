1be06efb8df93753fe030a359aad9946f0e93773
unknown <kaidofalcon@gmail.com>
2022-09-02 13:45:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { addTodo, removeTodo } from './Action/action';
 
 function App() {
+
+  const [ counter, setCounter ] = useState(0);
+  const [todo , setTodo ] = useState("");
+
+  const todos = useSelector(state => state.todos);
+
+  const dispatch = useDispatch();
+
+  const submitHandler = () => {
+    if (todo !== ""){
+      dispatch(addTodo(counter, todo))
+      setCounter(counter + 1);
+      setTodo("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>To-Do</h1>
+        <div className='add'>
+          <input 
+            type="text" 
+            value={todo}
+            placeholder= "enter Todo"
+            onChange= {(e) => setTodo(e.target.value)}
+          />
+
+        </div>
+        <button onClick={submitHandler}>Add</button>
+
+        <div className='remove'>
+          <ul>
+              {todos?.map((todo) =>
+              <li key={todo.id}>
+                <p>{todo.task}</p>
+                <button onClick={() => dispatch(removeTodo(todo.id))}>Remove</button>
+
+              </li>
+              )}
+          </ul>
+        </div>
+      </div>
+    </>
+    
   );
 }
 
